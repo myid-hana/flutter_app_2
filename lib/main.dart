a6eaecdbc5c9b7edaa47974d0de42d843b183d1a
myid-hana <myid-hana@github.com>
2021-05-27 18:24:11 +0900

    Drawer 메뉴 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -23,12 +23,6 @@ class MyHome extends StatelessWidget {
         title: Text('Appbar icon menu'),
         centerTitle: true,
         elevation: 0.0,
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            print('menu button is clicked');
-          },
-        ),
         actions: [
           IconButton(
             icon: Icon(Icons.shopping_cart),
@@ -39,6 +33,65 @@ class MyHome extends StatelessWidget {
             onPressed: () { print('search button is clicked'); },
           ),
         ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/img01.JPG'),
+              ),
+              otherAccountsPictures: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/img01.JPG'),
+                ),
+              ],
+              accountName: Text('박하나'),
+              accountEmail: Text('sosqkrgksml@mgmailnet'),
+              onDetailsPressed: () {
+                print('Arrow is moving');
+              },
+              decoration: BoxDecoration(
+                color: Colors.red[300],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0)
+                )
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home,
+              color: Colors.grey[850],
+              ),
+              title: Text('Home'),
+              onTap: () {
+                print('Home Tap');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings,
+              color: Colors.grey[850],
+              ),
+              title: Text('Settings'),
+              onTap: () {
+                print('Settings Tap');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer,
+              color: Colors.grey[850],
+              ),
+              title: Text('qna'),
+              onTap: () {
+                print('question_answer Tap');
+              },
+              trailing: Icon(Icons.add),
+            ),
+          ],
+        ),
       ),
     );
   }
