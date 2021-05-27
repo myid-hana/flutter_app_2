2c0e0688dd9f1b5fe0f5e51c6d6c28ce66941d93
myid-hana <myid-hana@github.com>
2021-05-27 13:50:40 +0900

    캐릭터 페이지 디자인

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,137 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void  main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'first app',
-      theme: ThemeData(
-        primarySwatch: Colors.green
-      ),
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      title: 'BBANTO',
+      home: Grade(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber[800],
       appBar: AppBar(
         title: Text('BBANTO'),
+        backgroundColor: Colors.amber[700],
         centerTitle: true,
-        backgroundColor: Colors.greenAccent,
         elevation: 0.0,
-      ),
-      body: Center( //가로축 정렬
-        child: Column( 
-            mainAxisAlignment: MainAxisAlignment.center, //세로축 정렬
+        ),
+        body: Padding( 
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('hello'),
-              Text('hello'),
-              Text('hello')
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/img01.JPG'),
+                  radius: 60.0,
+                ),
+              ),
+              Divider(
+                height: 60.0,
+                color: Colors.grey[800],
+                thickness: 0.5,
+                endIndent: 30.0,
+              ),
+              Text('NAME',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text('BBANTO',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold
+              ), 
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text('BBANTO POWER LEVEL',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text('14',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold
+              ), 
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                  width: 10.0,
+                  ),
+                  Text('using lightsaber',
+                  style: TextStyle( 
+                    fontSize: 16.0,
+                    letterSpacing: 1.0
+                  ),
+                  ),
+                ]
+              ),
+              Row(
+                children: [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                  width: 10.0,
+                  ),
+                  Text('face hero tatto',
+                  style: TextStyle( 
+                    fontSize: 16.0,
+                    letterSpacing: 1.0
+                  ),
+                  ),
+                ]
+              ),
+              Row(
+                children: [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                  width: 10.0,
+                  ),
+                  Text('fire flames',
+                  style: TextStyle( 
+                    fontSize: 16.0,
+                    letterSpacing: 1.0
+                  ),
+                  ),
+                ]
+              ),
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/img01.JPG'),
+                  radius: 40.0,
+                ),
+              )
             ],
-          ),
-      ),
-      
+            ),
+        ),
     );
   }
 }
