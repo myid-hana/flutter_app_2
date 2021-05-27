b43b5806c330223bd09c35c672b0ffebc56c3ef8
myid-hana <myid-hana@github.com>
2021-05-27 15:03:47 +0900

    사진, 텍스트 넣기 연습

--- lib/main.dart
+++ lib/main.dart
@@ -1,137 +1,88 @@
 import 'package:flutter/material.dart';
 
-void  main() => runApp(MyApp());
+void main() => runApp(HanaApp());
 
-class MyApp extends StatelessWidget {
+class HanaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'BBANT',
-      home: Grade(),
+      title: 'HanaApp',
+      home: HanaHome(),
     );
   }
 }
 
-class Grade extends StatelessWidget {
+class HanaHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[800],
+      backgroundColor: Colors.amber[100],
       appBar: AppBar(
-        title: Text('BBANTO'),
-        backgroundColor: Colors.amber[700],
+        title: Text('Hana App'),
         centerTitle: true,
-        elevation: 0.0,
-        ),
-        body: Padding( 
-          padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/img01.JPG'),
-                  radius: 60.0,
-                ),
+        backgroundColor: Colors.brown[600],
+        ), 
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 30.0, 10.0, 0.0),
+        child: Column(
+          children: [
+            Center(
+              child: CircleAvatar(
+              backgroundImage: AssetImage('assets/img01.JPG'),
+              radius: 60.0,
               ),
-              Divider(
-                height: 60.0,
-                color: Colors.grey[800],
-                thickness: 0.5,
-                endIndent: 30.0,
-              ),
-              Text('NAME',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Text('BBANTO',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold
-              ), 
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              Text('BBANTO POWER LEVEL',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Text('14',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold
-              ), 
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              Row(
-                children: [
-                  Icon(Icons.check_circle_outline),
-                  SizedBox(
-                  width: 10.0,
-                  ),
-                  Text('using lightsaber',
-                  style: TextStyle( 
-                    fontSize: 16.0,
-                    letterSpacing: 1.0
-                  ),
-                  ),
-                ]
-              ),
-              Row(
-                children: [
-                  Icon(Icons.check_circle_outline),
-                  SizedBox(
-                  width: 10.0,
-                  ),
-                  Text('face hero tatto',
-                  style: TextStyle( 
-                    fontSize: 16.0,
-                    letterSpacing: 1.0
-                  ),
-                  ),
-                ]
-              ),
-              Row(
-                children: [
-                  Icon(Icons.check_circle_outline),
-                  SizedBox(
-                  width: 10.0,
-                  ),
-                  Text('fire flames',
-                  style: TextStyle( 
-                    fontSize: 16.0,
-                    letterSpacing: 1.0
-                  ),
-                  ),
-                ]
-              ),
-              Center(
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/img01.JPG'),
-                  radius: 40.0,
-                ),
-              )
-            ],
+            ), 
+            SizedBox(
+              height: 20.0,
             ),
+            Text('안녕하세요 박하나입니다.',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0
+              ),
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('flutter Study를 열심히 하자!',
+                      style: TextStyle(fontSize: 18.0),
+                      ),
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('앞으로 2개월만',
+                      style: TextStyle(fontSize: 18.0),
+                      ),
+              ],
+            ),
+            SizedBox(height: 20.0,),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('일에만 집중하자.',
+                      style: TextStyle(fontSize: 18.0),
+                      ),
+              ],
+            ),
+          ],    
         ),
+      )
     );
   }
 }
